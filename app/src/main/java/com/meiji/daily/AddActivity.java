34b9017e6eda3bd3427a991ea52a7efbcb91ed34
iMeiji <361183736@qq.com>
2016-12-01 13:33:23 +0800

    add .travis.yml

---
+++ app/src/main/java/com/meiji/daily/AddActivity.java
@@ -0,0 +1,12 @@
+package com.meiji.daily;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Meiji on 2016/12/1.
+ */
+
+public class AddActivity extends AppCompatActivity {
+
+
+}
