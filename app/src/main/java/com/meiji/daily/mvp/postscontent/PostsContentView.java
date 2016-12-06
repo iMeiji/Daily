529907b5f637675075c6de5c6a8b20f42b9bc87d
iMeiji <361183736@qq.com>
2016-12-06 19:20:07 +0800

    apk压缩体积

--- app/src/main/java/com/meiji/daily/mvp/postscontent/PostsContentView.java
+++ app/src/main/java/com/meiji/daily/mvp/postscontent/PostsContentView.java
@@ -8,6 +8,7 @@ import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.KeyEvent;
@@ -118,8 +119,9 @@ public class PostsContentView extends BaseActivity implements View.OnClickListen
         toolbar_layout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_layout);
 
         setSupportActionBar(toolbar_title);
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
         fab_share.setOnClickListener(this);
