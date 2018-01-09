7e2aac836d7512fefcc4c96c24f2599af9754b10
iMeiji <361183736@qq.com>
2018-01-09 14:43:09 +0800

    Update MultiDex

--- app/src/main/java/com/meiji/daily/App.java
+++ app/src/main/java/com/meiji/daily/App.java
@@ -1,7 +1,9 @@
 package com.meiji.daily;
 
+import android.app.Application;
 import android.content.Context;
-import android.support.multidex.MultiDexApplication;
+import android.os.Build;
+import android.support.multidex.MultiDex;
 
 import com.crashlytics.android.Crashlytics;
 import com.crashlytics.android.core.CrashlyticsCore;
@@ -14,10 +16,19 @@ import io.fabric.sdk.android.Fabric;
  * Created by Meiji on 2016/12/7.
  */
 
-public class App extends MultiDexApplication {
+public class App extends Application {
 
     public static Context sAppContext;
     public static AppComponent sAppComponent;
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        // Android 5.0 之前版本的 Dalvik 可执行文件分包支持
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            MultiDex.install(this);
+        }
+    }
 
     @Override
     public void onCreate() {
